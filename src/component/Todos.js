0fc5aaad6472a08edc3b0ef3be18c5c164bfb73d
Dmitry <61842792+dimonkac@users.noreply.github.com>
2021-08-10 19:54:44 +0300

    final work

--- src/component/Todos.js
+++ src/component/Todos.js
@@ -3,5 +3,4 @@ export const Todos = [
   { task: "Покорми кота", status: false, id: 2 },
   { task: "Выйти на улицу", status: false, id: 3 },
   { task: "Съесть пломбир", status: false, id: 4 },
-  { task: "Купить цветы", status: false, id: 5 },
 ];
